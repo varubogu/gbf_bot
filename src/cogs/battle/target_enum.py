7f3915cf3f248303acc670d3270135c49cafaa52
varubogu <toyosukevarubogu@gmail.com>
2023-10-02 14:39:55 +0000

    列の順番変更

--- src/cogs/battle/target_enum.py
+++ src/cogs/battle/target_enum.py
@@ -3,26 +3,26 @@ from cogs.battle.battle_type import BattleTypeEnum as BT
 
 
 class Target(Enum):
-    PROTO_BAHAMUT_HL = (1, 6, [BT.DEFAULT], "プロトバハムートHL", "つよバハ")
-    ULTIMATE_BAHAMUT = (2, 6, [BT.DEFAULT], "進撃せし究極の竜HL", "アルバハHL")
-    LUCIFER = (3, 6, [BT.DEFAULT, BT.ALL_ELEMENT, BT.SYSTEM], "ダークラプチャー(Hard)", "ルシH")
-    BEELZEBUB = (4, 6, [BT.DEFAULT, BT.SYSTEM], "バース・オブ・ニューキング", "ベルゼバブ")
-    BELIAL = (5, 6, [BT.DEFAULT, BT.ALL_ELEMENT], "狡智の堕天使", "ベリアル")
-    SUPER_ULTIMATE_BAHAMUT = (6, 6, [BT.DEFAULT, BT.ALL_ELEMENT, BT.SYSTEM], "進撃せし蒼き究極の竜", "スパバハ")
+    PROTO_BAHAMUT_HL = (1, 6, "プロトバハムートHL", "つよバハ", [BT.DEFAULT])
+    ULTIMATE_BAHAMUT = (2, 6, "進撃せし究極の竜HL", "アルバハHL", [BT.DEFAULT])
+    LUCIFER = (3, 6, "ダークラプチャー(Hard)", "ルシH", [BT.DEFAULT, BT.ALL_ELEMENT, BT.SYSTEM])
+    BEELZEBUB = (4, 6, "バース・オブ・ニューキング", "ベルゼバブ", [BT.DEFAULT, BT.SYSTEM])
+    BELIAL = (5, 6, "狡智の堕天使", "ベリアル", [BT.DEFAULT, BT.ALL_ELEMENT])
+    SUPER_ULTIMATE_BAHAMUT = (6, 6, "進撃せし蒼き究極の竜", "スパバハ", [BT.DEFAULT, BT.ALL_ELEMENT, BT.SYSTEM])
 
     def __init__(
             self,
             target_id: int,
             recruit_count: int,
-            use_battle_type: [int],
             quest_name: str,
-            quest_alias: str
+            quest_alias: str,
+            use_battle_type: [int]
     ):
         self.__target_id = target_id
         self.__recruit_count = recruit_count
-        self.__use_battle_type = use_battle_type
         self.__quest_name = quest_name
         self.__quest_alias = quest_alias
+        self.__use_battle_type = use_battle_type
 
     @property
     def target_id(self) -> int:
@@ -33,16 +33,16 @@ class Target(Enum):
         return self.__recruit_count
 
     @property
-    def use_battle_type(self) -> [int]:
-        return self.__use_battle_type
-
-    @property
     def quest_name(self) -> str:
         return self.__quest_name
 
     @property
     def quest_alias(self) -> str:
         return self.__quest_alias
+
+    @property
+    def use_battle_type(self) -> [int]:
+        return self.__use_battle_type
 
     @classmethod
     def find_target(cls, target_id_) -> 'Target':
