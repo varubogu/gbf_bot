58d2cde04e330c86d4df0eacd504d09121e77dcd
varubogu <toyosukevarubogu@gmail.com>
2023-10-13 08:17:41 +0900

    環境変数シングルトンのテストを微修正

--- tests/unit/gbf/environment/test_environment_singleton.py
+++ tests/unit/gbf/environment/test_environment_singleton.py
@@ -2,42 +2,31 @@ import pytest
 from gbf.environment.environment_singleton import EnvironmentSingleton
 
 
-@pytest.fixture
-async def setup():
-    env_singleton = EnvironmentSingleton()
-    await env_singleton.clear()
-    await env_singleton.set('TEST1', '1')
-    await env_singleton.set('TEST2', '2')
+class TestEnvironmentSingleton:
 
-    yield
+    @pytest.mark.asyncio
+    async def test_singleton(self):
+        s1 = EnvironmentSingleton()
+        s2 = EnvironmentSingleton()
+        assert s1 is s2
 
-    await env_singleton.clear()
+    @pytest.mark.asyncio
+    async def test_environment_eval(self):
 
+        env_singleton = EnvironmentSingleton()
 
-def test_singleton():
-    s1 = EnvironmentSingleton()
-    s2 = EnvironmentSingleton()
-    assert s1 is s2
+        await env_singleton.clear()
+        await env_singleton.set('TEST1', '1')
+        await env_singleton.set('TEST2', '2')
 
+        actual = await env_singleton.replace_env_eval('abc')
+        assert actual == 'abc'
 
-@pytest.mark.asyncio
-async def test_environment_eval():
+        actual = await env_singleton.replace_env_eval('ab${TEST1}c')
+        assert actual == 'ab1c'
 
-    env_singleton = EnvironmentSingleton()
-    await env_singleton.clear()
-    await env_singleton.set('TEST1', '1')
-    await env_singleton.set('TEST2', '2')
+        actual = await env_singleton.replace_env_eval('ab${TEST1}cd${TEST1}e')
+        assert actual == 'ab1cd1e'
 
-    actual = await env_singleton.replace_env_eval('abc')
-    assert actual == 'abc'
-
-    actual = await env_singleton.replace_env_eval('ab${TEST1}c')
-    assert actual == 'ab1c'
-
-    actual = await env_singleton.replace_env_eval('ab${TEST1}cd${TEST1}e')
-    assert actual == 'ab1cd1e'
-
-    actual = await env_singleton.replace_env_eval('ab${TEST1}cd${TEST2}e')
-    assert actual == 'ab1cd2e'
-
-    await env_singleton.clear()
+        actual = await env_singleton.replace_env_eval('ab${TEST1}cd${TEST2}e')
+        assert actual == 'ab1cd2e'
