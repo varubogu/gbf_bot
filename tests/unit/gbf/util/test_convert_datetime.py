d53ad65be096333077507a11d7eb91b68a0ac407
varubogu <toyosukevarubogu@gmail.com>
2023-10-12 10:39:49 +0900

    unit testパターン追加

--- tests/unit/gbf/util/test_convert_datetime.py
+++ tests/unit/gbf/util/test_convert_datetime.py
@@ -16,3 +16,31 @@ async def test_convert_datetime():
 
     actual = await cdt("2022-01-01 12:00")
     assert actual == datetime(2022, 1, 1, 12, 0)
+
+
+@pytest.mark.asyncio
+async def test_exception():
+
+    # none
+    x = await cdt(None)
+    assert x is None
+
+    # empty
+    x = await cdt('')
+    assert x is None
+
+    # date
+    with pytest.raises(ValueError):
+        await cdt('2023/1/32')
+
+    # invalid hour
+    with pytest.raises(ValueError):
+        await cdt('25:00')
+
+    # invalid minute
+    with pytest.raises(ValueError):
+        await cdt('10:60')
+
+    # invalid second
+    with pytest.raises(ValueError):
+        await cdt('23:59:60')
--- tests/unit/gbf/util/test_convert_time.py
+++ tests/unit/gbf/util/test_convert_time.py
@@ -13,6 +13,17 @@ async def test_hour_minute():
 
 
 @pytest.mark.asyncio
+async def test_hour_minute_from_date():
+    from_date = datetime(2022, 12, 31, 23, 59, 59)
+
+    actual = await convert_time('0:00', from_date)
+    assert actual == datetime(2022, 12, 31, 0, 0, 0)
+
+    actual = await convert_time('1:23', from_date)
+    assert actual == datetime(2022, 12, 31, 1, 23)
+
+
+@pytest.mark.asyncio
 async def test_hour_minute_second():
     actual = await convert_time('0:00:00')
     assert actual == datetime.min
@@ -22,6 +33,20 @@ async def test_hour_minute_second():
 
     actual = await convert_time('4:56:12')
     assert actual == datetime.min.replace(hour=4, minute=56, second=12)
+
+
+@pytest.mark.asyncio
+async def test_hour_minute_second_from_date():
+    from_date = datetime(2022, 12, 31, 23, 59, 59)
+
+    actual = await convert_time('0:00:00', from_date)
+    assert actual == datetime(2022, 12, 31, 0, 0, 0)
+
+    actual = await convert_time('00:00:00', from_date)
+    assert actual == datetime(2022, 12, 31, 0, 0, 0)
+
+    actual = await convert_time('4:56:12', from_date)
+    assert actual == datetime(2022, 12, 31, 4, 56, 12)
 
 
 @pytest.mark.asyncio
@@ -37,7 +62,15 @@ async def test_exception():
 
     # date
     with pytest.raises(ValueError):
-        await convert_time('2023/1/1')
+        await convert_time('0/1/1')
+
+    # date
+    with pytest.raises(ValueError):
+        await convert_time('2023/13/1')
+
+    # date
+    with pytest.raises(ValueError):
+        await convert_time('2023/1/32')
 
     # invalid hour
     with pytest.raises(ValueError):
