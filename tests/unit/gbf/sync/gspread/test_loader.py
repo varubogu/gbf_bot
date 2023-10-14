fa4a0bfce88b4ae17ca366d6680b5c30e8a7ed98
varubogu <toyosukevarubogu@gmail.com>
2023-10-14 11:22:47 +0900

    GSpreadLoadのテストを作成

---
+++ tests/unit/gbf/sync/gspread/test_loader.py
@@ -0,0 +1,52 @@
+from datetime import datetime
+import uuid
+import pytest
+from gbf.sync.gspread.loader import GSpreadLoader
+from gbf.sync.gspread.table_definition import GSpreadTableDefinition
+from sqlalchemy import Column, Integer, BigInteger, String, DateTime, Uuid
+
+
+class TestGSpreadLoader():
+
+    @pytest.fixture()
+    def loader(self) -> GSpreadLoader:
+        return GSpreadLoader()
+
+    async def test_convert_table(self):
+        pass
+
+    async def test_convert_row(self):
+        pass
+
+    @pytest.mark.asyncio
+    async def test_convert_value_int(self, loader):
+        actual = await loader.convert_value("1", Column(Integer))
+        assert actual == 1 and isinstance(actual, int)
+
+        actual = await loader.convert_value("1", Column(BigInteger))
+        assert actual == 1 and isinstance(actual, int)
+
+    @pytest.mark.asyncio
+    async def test_convert_value_str(self, loader):
+        actual = await loader.convert_value("1", Column(String))
+        assert actual == "1" and isinstance(actual, str)
+
+    @pytest.mark.asyncio
+    async def test_convert_value_datetime(self, loader):
+        loader = GSpreadLoader()
+        dt_str = "2023/12/31"
+        actual = await loader.convert_value(dt_str, Column(DateTime))
+        expected = datetime(2023, 12, 31)
+        assert actual == expected and isinstance(actual, datetime)
+
+        dt_str = "2023/12/31 23:59:59"
+        actual = await loader.convert_value(dt_str, Column(DateTime))
+        expected = datetime(2023, 12, 31, 23, 59, 59)
+        assert actual == expected and isinstance(actual, datetime)
+
+    @pytest.mark.asyncio
+    async def test_convert_value_uuid(self, loader):
+        actual_str = 'cdc1a3ca-6365-485c-b8e0-7e6a5d5ac5aa'
+        actual = await loader.convert_value(actual_str, Column(Uuid))
+        expected = uuid.UUID(actual_str)
+        assert actual == expected and isinstance(actual, uuid.UUID)
