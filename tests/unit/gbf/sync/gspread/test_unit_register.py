7df9b3438e5413aae9a51dd488427658ac63e390
varubogu <toyosukevarubogu@gmail.com>
2023-10-15 09:18:25 +0900

    TestUnitGspreadRegisterをfixtureを使って書き直した

--- tests/unit/gbf/sync/gspread/test_unit_register.py
+++ tests/unit/gbf/sync/gspread/test_unit_register.py
@@ -6,34 +6,42 @@ from gbf.sync.gspread.register import GSpreadRegister
 
 class TestUnitGspreadRegister:
 
-    @pytest.mark.asyncio
-    async def test_convert_cell_int(self):
-        register = GSpreadRegister()
+    @pytest.fixture
+    def gspread_register(self) -> GSpreadRegister:
+        return GSpreadRegister()
 
-        cell = await register.convert_cell(1)
+    @pytest.mark.asyncio
+    async def test_convert_cell_int(
+        self,
+        gspread_register: GSpreadRegister
+    ):
+        cell = await gspread_register.convert_cell(1)
         assert cell == 1 and type(cell) is int
 
     @pytest.mark.asyncio
-    async def test_convert_cell_str(self):
-        register = GSpreadRegister()
-
-        cell = await register.convert_cell('1')
+    async def test_convert_cell_str(
+        self,
+        gspread_register: GSpreadRegister
+    ):
+        cell = await gspread_register.convert_cell('1')
         assert cell == '1' and type(cell) is str
 
     @pytest.mark.asyncio
-    async def test_convert_cell_datetime(self):
-        register = GSpreadRegister()
-
+    async def test_convert_cell_datetime(
+        self,
+        gspread_register: GSpreadRegister
+    ):
         dt = datetime(2023, 12, 31, 23, 59, 59)
         dt_str = dt.strftime('%Y-%m-%d %H:%M:%S')
-        cell = await register.convert_cell(dt)
+        cell = await gspread_register.convert_cell(dt)
         assert cell == dt_str and type(cell) is str
 
     @pytest.mark.asyncio
-    async def test_convert_cell_uuid(self):
-        register = GSpreadRegister()
-
+    async def test_convert_cell_uuid(
+        self,
+        gspread_register: GSpreadRegister
+    ):
         uuid_value = uuid.uuid4()
         uuid_str = str(uuid_value)
-        cell = await register.convert_cell(uuid_value)
+        cell = await gspread_register.convert_cell(uuid_value)
         assert cell == uuid_str and type(cell) is str
