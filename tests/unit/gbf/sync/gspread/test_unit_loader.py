6647c2670bffba8b93c04ff437072c15c9455da2
varubogu <toyosukevarubogu@gmail.com>
2023-10-15 09:19:37 +0900

    TestUnitGSpreadLoaderのfixtureの命名を修正

--- tests/unit/gbf/sync/gspread/test_unit_loader.py
+++ tests/unit/gbf/sync/gspread/test_unit_loader.py
@@ -8,38 +8,38 @@ from sqlalchemy import Column, Integer, BigInteger, String, DateTime, Uuid
 class TestUnitGSpreadLoader():
 
     @pytest.fixture()
-    def loader(self) -> GSpreadLoader:
+    def gspread_loader(self) -> GSpreadLoader:
         return GSpreadLoader()
 
     @pytest.mark.asyncio
-    async def test_convert_value_int(self, loader):
-        actual = await loader.convert_value("1", Column(Integer))
+    async def test_convert_value_int(self, gspread_loader):
+        actual = await gspread_loader.convert_value("1", Column(Integer))
         assert actual == 1 and isinstance(actual, int)
 
-        actual = await loader.convert_value("1", Column(BigInteger))
+        actual = await gspread_loader.convert_value("1", Column(BigInteger))
         assert actual == 1 and isinstance(actual, int)
 
     @pytest.mark.asyncio
-    async def test_convert_value_str(self, loader):
-        actual = await loader.convert_value("1", Column(String))
+    async def test_convert_value_str(self, gspread_loader):
+        actual = await gspread_loader.convert_value("1", Column(String))
         assert actual == "1" and isinstance(actual, str)
 
     @pytest.mark.asyncio
-    async def test_convert_value_datetime(self, loader):
-        loader = GSpreadLoader()
+    async def test_convert_value_datetime(self, gspread_loader):
+        gspread_loader = GSpreadLoader()
         dt_str = "2023/12/31"
-        actual = await loader.convert_value(dt_str, Column(DateTime))
+        actual = await gspread_loader.convert_value(dt_str, Column(DateTime))
         expected = datetime(2023, 12, 31)
         assert actual == expected and isinstance(actual, datetime)
 
         dt_str = "2023/12/31 23:59:59"
-        actual = await loader.convert_value(dt_str, Column(DateTime))
+        actual = await gspread_loader.convert_value(dt_str, Column(DateTime))
         expected = datetime(2023, 12, 31, 23, 59, 59)
         assert actual == expected and isinstance(actual, datetime)
 
     @pytest.mark.asyncio
-    async def test_convert_value_uuid(self, loader):
+    async def test_convert_value_uuid(self, gspread_loader):
         actual_str = 'cdc1a3ca-6365-485c-b8e0-7e6a5d5ac5aa'
-        actual = await loader.convert_value(actual_str, Column(Uuid))
+        actual = await gspread_loader.convert_value(actual_str, Column(Uuid))
         expected = uuid.UUID(actual_str)
         assert actual == expected and isinstance(actual, uuid.UUID)
