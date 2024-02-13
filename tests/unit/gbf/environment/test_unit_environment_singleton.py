1e877acedabba6fb583e4754f631e3c03511ffad
varubogu <toyosukevarubogu@gmail.com>
2024-02-13 16:30:35 +0900

    環境変数シングルトンのテストを修正

--- tests/unit/gbf/environment/test_unit_environment_singleton.py
+++ tests/unit/gbf/environment/test_unit_environment_singleton.py
@@ -21,49 +21,114 @@ class TestUnitEnvironmentSingleton:
         assert s1 is s2
 
     @pytest.mark.asyncio
-    async def test_environment_eval(
+    async def test_set_get(
             self,
             env_singleton: EnvironmentSingleton
     ):
+        # 追加
+        key = "TEST_SET_GET"
+        value1 = "KEY1"
+        await env_singleton.set(key, value1)
+        actual = await env_singleton.get(key)
+        assert actual == value1
 
+        # 上書き
+        value2 = "KEY2"
+        await env_singleton.set(key, value2)
+        actual = await env_singleton.get(key)
+        assert actual == value2
+
+    @pytest.mark.asyncio
+    async def test_get_default(
+            self,
+            env_singleton: EnvironmentSingleton
+    ):
+        # デフォルト指定無し
+        key = "TEST_GET_DEFAULT"
+        actual = await env_singleton.get(key)
+        assert actual is None
+
+        # デフォルト指定有り
+        value2 = "VALUE2"
+        actual = await env_singleton.get(key, value2)
+        assert actual == value2
+
+    @pytest.mark.asyncio
+    async def test_delete(
+            self,
+            env_singleton: EnvironmentSingleton
+    ):
+        # データありの状態にする
+        key = "TEST_DELETE"
+        value1 = "DELETE1"
+        await env_singleton.set(key, value1)
+        actual = await env_singleton.get(key)
+        assert actual == value1
+
+        await env_singleton.delete(key)
+        actual = await env_singleton.get(key)
+        assert actual is None
+
+    @pytest.mark.asyncio
+    async def test_clear(
+            self,
+            env_singleton: EnvironmentSingleton
+    ):
+        # データありの状態にする
+        assert len(env_singleton._variables) == 2
+
+        await env_singleton.clear()
+        assert len(env_singleton._variables) == 0
+
+    @pytest.mark.asyncio
+    async def test_replace_env_eval(
+            self,
+            env_singleton: EnvironmentSingleton
+    ):
+        # 変数無し
         actual = await env_singleton.replace_env_eval('abc')
         assert actual == 'abc'
 
+        # 変数１つ
         actual = await env_singleton.replace_env_eval('ab${TEST1}c')
         assert actual == 'ab1c'
 
+        # 変数１種類２回
         actual = await env_singleton.replace_env_eval('ab${TEST1}cd${TEST1}e')
         assert actual == 'ab1cd1e'
 
+        # 変数２種類
         actual = await env_singleton.replace_env_eval('ab${TEST1}cd${TEST2}e')
         assert actual == 'ab1cd2e'
 
     @pytest.mark.asyncio
-    async def test_find_env_keys_empty(
+    async def test_find_env_keys(
             self,
             env_singleton: EnvironmentSingleton
     ):
+        # マッチ無し
         a = 'abcdef'
         actual = await env_singleton.find_env_keys(a)
         assert len(actual) == 0
 
-    @pytest.mark.asyncio
-    async def test_find_env_keys_match(
-            self,
-            env_singleton: EnvironmentSingleton
-    ):
+        # １つマッチ
         a = 'abc${TEST1}def'
         actual = await env_singleton.find_env_keys(a)
         assert len(actual) == 1
+        assert actual[0] == "${TEST1}"
 
+        # ２つマッチ
         a = 'abc${TEST1}de${TEST2}f'
         actual = await env_singleton.find_env_keys(a)
         assert len(actual) == 2
+        assert actual[0] == "${TEST1}"
+        assert actual[1] == "${TEST2}"
 
         # '$'が無い場合
         a = 'abc${TEST1}de{TEST2}f'
         actual = await env_singleton.find_env_keys(a)
         assert len(actual) == 1
+        assert actual[0] == "${TEST1}"
 
     @pytest.mark.asyncio
     async def test_substr_env_key(
